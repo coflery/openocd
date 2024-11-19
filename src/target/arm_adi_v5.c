545d56f7c228dffbfa85cf22d0baccd7fe0795e4
Frand Ren <coflery@gmail.com>
2024-11-19 20:59:31 +0800

    add stlinkv3 and stm32h5x/stm32h7x/stm32l4x

--- src/target/arm_adi_v5.c
+++ src/target/arm_adi_v5.c
@@ -1561,6 +1561,8 @@ static const struct dap_part_nums {
 	{ 0x1eb,  0x211, "Tegra 210 ROM",              "(ROM Table)", },
 	{ 0x1eb,  0x302, "Denver Debug",               "(Debug Unit)", },
 	{ 0x1eb,  0x402, "Denver PMU",                 "(Performance Monitor Unit)", },
+	{ 0x020,  0x484, "STM32H56/H57xx",             "(STM32H5 Microcontroller)", },
+	{ 0x020,  0x474, "STM32H50xx",                 "(STM32H5 Microcontroller)", },
 	{ 0x575,  0x132, "STAR SCS",                   "(System Control Space)", },
 	{ 0x575,  0x4d2, "Cortex-M52 ROM",             "(ROM Table)", },
 	{ 0x575,  0xd24, "Cortex-M52 Debug",           "(Debug Unit)", },
